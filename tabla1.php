2ceda389b4e43404173c0ae0cb3c3559e50721f3
HugoCD <cdhugo9@gmail.com>
2024-06-20 23:26:32 -0600

    tabla 1 agregada

---
+++ tabla1.php
@@ -0,0 +1,37 @@
+<table>
+        <thead>
+            <tr>
+                <th>Estados</th>
+                <th>0</th>
+                <th>1</th>
+                <th>X</th>
+                <th>B</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>q0</td>
+                <td>(q1,B,R)</td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td>q1</td>
+                <td>(q1,B,R)</td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td>John Smith</td>
+                <td>35</td>
+                <td>Estados Unidos</td>
+            </tr>
+            <tr>
+                <td>Lisa Müller</td>
+                <td>28</td>
+                <td>Alemania</td>
+            </tr>
+        </tbody>
+    </table>
